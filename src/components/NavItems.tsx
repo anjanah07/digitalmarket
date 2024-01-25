a38d1aec3a4af72e4e46b618a33b3ce70f5fc6a7
anjanah07 <thanjana7@gmail.com>
2024-01-25 10:35:11 +0530

    add: outside click & esc event listener

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,14 +1,30 @@
 "use client";
 
 import { PRODUCT_CATGORIES } from "@/config";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeindex, setActiveIndex] = useState<null | number>(null);
   const isAnyOpen = activeindex !== null;
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handler);
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
   return (
-    <div className="flex h-full gap-4">
+    <div className="flex h-full gap-4" ref={navRef}>
       {PRODUCT_CATGORIES.map((category, index) => {
         const handleOpen = () => {
           if (activeindex === index) {
